9e7e4b57c32530cc9ff5beecf467c9ce59bfed26
LudivineHay <ludivine.hay.13@gmail.com>
2019-01-31 12:32:28 +0100

    implement ideas in html via script.js

--- script.js
+++ script.js
@@ -12,4 +12,22 @@ import "./style.scss";
   Put the JavaScript code you want below.
 */
 
-console.log("Hey look in your browser console. It works!");
+let ideaCount = localStorage.getItem("ideaCount");
+if(ideaCount === null){
+	ideaCount = 0;
+} else {
+	ideaCount = parseInt(ideaCount);
+}
+
+let idea = JSON.parse(localStorage.getItem("idea0"));
+
+let ideasElt = document.querySelector(".ideas");
+ideasElt.querySelector(".idea-name").innerText = idea.name;
+ideasElt.querySelector(".idea-description").innerText = idea.description;
+
+let modalElt = document.querySelector(".modal-content .ideas");
+modalElt.querySelector(".idea-name").innerText = idea.name;
+modalElt.querySelector(".idea-description").innerText = idea.description;
+modalElt.querySelector(".details").innerText = idea.details;
+modalElt.querySelector(".comments").innerText = JSON.stringify(idea.comments);
+
