03f316fd7685c84dc0f28c28494c207fb90a33be
Louis <cantinieauxlouis@gmail.com>
2019-01-31 14:58:05 +0100

    Implement modal window it works

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 // import some polyfill to ensure everything works OK
 import "babel-polyfill"
 
+//import JQuery
+import $ from "jquery";
+
 // import bootstrap's javascript part
 import 'bootstrap';
 
 // import the style
 import "./style.scss";
+
 
 /*
   Put the JavaScript code you want below.
@@ -25,9 +29,14 @@ let ideasElt = document.querySelector(".ideas");
 ideasElt.querySelector(".idea-name").innerText = idea.name;
 ideasElt.querySelector(".idea-description").innerText = idea.description;
 
+let modalHead = document.querySelector(".modal-title");
+modalHead.innerText = idea.name;
+
 let modalElt = document.querySelector(".modal-content .ideas");
-modalElt.querySelector(".idea-name").innerText = idea.name;
 modalElt.querySelector(".idea-description").innerText = idea.description;
 modalElt.querySelector(".details").innerText = idea.details;
 modalElt.querySelector(".comments").innerText = JSON.stringify(idea.comments);
 
+console.log(ideasElt);
+
+ideasElt.addEventListener("click", () => $("#exampleModalCenter").modal("show"));
