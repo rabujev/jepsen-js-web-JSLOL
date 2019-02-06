c44755333ee13dffa21a28482f05950c9c016581
Samuel Degueldre <samuelfdegueldre@gmail.com>
2019-02-06 10:32:31 +0100

    Minor refactor and comments

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-
-// import some polyfill to ensure everything works OK
 import "babel-polyfill";
-
+import $ from "jquery";
+import 'bootstrap';
 import {markdown} from 'markdown';
 
-//import JQuery
-import $ from "jquery";
-
-// import bootstrap's javascript part
-import 'bootstrap';
-
-// import the style
 import "./style.scss";
 
-
-/*
-  Put the JavaScript code you want below.
-*/
+/*---------------
+ * Idea-loading
+ *---------------*/
 
 let ideas = JSON.parse(localStorage.getItem('ideas'));
 if (ideas === null){
   ideas = [];
 }
 
-/*---------------
- * Idea-loading
- *---------------*/
 let content = document.querySelector('.content');
 
 for(let i = 0; i < ideas.length; i++){
@@ -33,23 +21,28 @@ for(let i = 0; i < ideas.length; i++){
 
   let ideaNode = document.createElement('div');
   ideaNode.classList = 'idea';
+
   let name = document.createElement('h2');
   name.classList = 'idea-name';
   name.innerText = idea.name
-  ideaNode.appendChild(name);
+
   let description = document.createElement('p');
   description.classList = 'idea-description';
   description.innerHTML = markdown.toHTML(idea.description);
+
+  ideaNode.appendChild(name);
   ideaNode.appendChild(description);
   content.appendChild(ideaNode);
-
-  console.log(idea);
 
   ideaNode.addEventListener("click", () =>  {
     loadDetails(i)
     $("#idea-detail-modal").modal("show");
   });
 }
+
+/*---------------
+ * Details
+ *---------------*/
 
 let detailsModal = document.querySelector("#idea-detail-modal");
 let detailsHTML = detailsModal.innerHTML;
@@ -62,21 +55,10 @@ function loadDetails(ideaNumber){
   detailsModal.querySelector(".idea-description").innerHTML = markdown.toHTML(idea.description);
   detailsModal.querySelector(".details").innerHTML = markdown.toHTML(idea.details);
 
-  console.log(detailsModal.querySelector('.add-comment'));
-
   let commentWindow = document.querySelector(".comment-creation");
   let commentBtn = commentWindow.querySelector("button");
-  commentWindow.querySelector(".author").addEventListener("input", ()=>{
-    if(commentWindow.querySelector(".author").value === "" ||
-       commentWindow.querySelector(".comment-text").value === "")
-    {
-      commentBtn.disabled = true;
-    } else {
-      commentBtn.disabled = false;
-    }
-  });
 
-    commentWindow.querySelector(".comment-text").addEventListener("input", ()=>{
+  let updateCommentButtonState = () => {
     console.log("event lancé");
     if(commentWindow.querySelector(".author").value === "" ||
        commentWindow.querySelector(".comment-text").value === "")
@@ -85,33 +67,33 @@ function loadDetails(ideaNumber){
     } else {
       commentBtn.disabled = false;
     }
-  });
+  }
+  commentWindow.querySelector(".author").addEventListener("input", updateCommentButtonState);
+  commentWindow.querySelector(".comment-text").addEventListener("input", updateCommentButtonState);
 
   detailsModal.querySelector('.add-comment').addEventListener('click', () => {
     addComment(ideaNumber);
   });
 
-
   let commentsNode = document.querySelector(".comments");
-
-  for(let i = 0; i < idea.comments.length; i++){
+  for(let comment of idea.comments){
   	//creation du div commentaire
-  	let comment = document.createElement("div");
-    comment.classList = "comment";
+  	let commentDiv = document.createElement("div");
+    commentDiv.classList = "comment";
 
   	// creation du div nom d'auteur dans le div commentaire
   	let author = document.createElement("div");
-  	author.innerText = idea.comments[i].author + ':';
+  	author.innerText = comment.author + ':';
     author.classList = "comment-author";
-  	comment.appendChild(author);
+  	commentDiv.appendChild(author);
 
   	// creation du div texte dans le div commentaire
   	let commentText = document.createElement("div");
-  	commentText.innerHTML = markdown.toHTML(idea.comments[i].text);
+  	commentText.innerHTML = markdown.toHTML(comment.text);
     commentText.classList = "comment-content";
-  	comment.appendChild(commentText);
+  	commentDiv.appendChild(commentText);
 
-    commentsNode.appendChild(comment);
+    commentsNode.appendChild(commentDiv);
   }
 
   detailsModal.querySelector(".edit-btn").addEventListener("click", () => {
@@ -127,12 +109,7 @@ function loadDetails(ideaNumber){
  * Idea-creation
  *---------------*/
 
-document.querySelector('.idea-creation').addEventListener('click', () => {
-  $('#idea-creation-modal').modal('show');
-});
-
 let creationModal = document.querySelector('#idea-creation-modal');
-
 creationModal.querySelector("#submit-idea").addEventListener('click', () => {
   let ideaName = creationModal.querySelector('.idea-name');
   let ideaDescription = creationModal.querySelector('.idea-description');
@@ -154,6 +131,9 @@ creationModal.querySelector("#submit-idea").addEventListener('click', () => {
   window.location.reload();
 });
 
+/*---------------
+ * Ideas storage
+ *---------------*/
 
 function storeIdea(idea, ideaNumber) {
   if(typeof ideaNumber === "undefined"){
@@ -163,7 +143,6 @@ function storeIdea(idea, ideaNumber) {
   }
 
   localStorage.setItem('ideas', JSON.stringify(ideas));
-
 }
 
 
@@ -173,14 +152,11 @@ function storeIdea(idea, ideaNumber) {
 
 function editDetails(ideaNumber){
   let idea = ideas[ideaNumber];
-
   detailsModal.innerHTML = creationModal.innerHTML;
 
   detailsModal.querySelector('.idea-name').value = idea.name;
   detailsModal.querySelector(".idea-description").value = idea.description;
   detailsModal.querySelector(".detailed-description").value = idea.details;
-
-
   detailsModal.querySelector("#submit-idea").remove();
 
   let editBtn = document.createElement("button");
@@ -220,59 +196,58 @@ function deleteIdea(ideaNumber){
   confirmBtn.classList = "btn btn-primary btn-danger";
   confirmBtn.innerText = "Confirm deletion";
 
-  let footer = detailsModal.querySelector(".modal-footer");
-  let closeButton = detailsModal.querySelector(".close-modal");
-
-  footer.querySelector(".edit-btn").style.display="none";
-  footer.querySelector(".delete-btn").style.display="none";
-  footer.querySelector(".invisible").style.display="none";
-
-
   confirmBtn.addEventListener("click", () => {
     ideas.splice(ideaNumber,1);
     localStorage.setItem('ideas', JSON.stringify(ideas));
     window.location.reload();
   });
 
+  let footer = detailsModal.querySelector(".modal-footer");
+  let closeButton = footer.querySelector(".close-modal");
+
+  footer.querySelector(".edit-btn").style.display = "none";
+  footer.querySelector(".delete-btn").style.display = "none";
+  footer.querySelector(".invisible").style.display = "none";
+
   footer.insertBefore(confirmBtn, closeButton);
 }
 
 
 /*---------------
- * Deleting idea
+ * Adding comments
  *---------------*/
 
- function addComment(ideaNumber){
-   console.log('Called addComment');
-   let idea = ideas[ideaNumber];
-   console.log(idea);
+function addComment(ideaNumber){
+  let idea = ideas[ideaNumber];
 
-   let comment = {
-     author: detailsModal.querySelector('.author').value,
-     text: detailsModal.querySelector('.comment-text').value,
-   }
+  let comment = {
+    author: detailsModal.querySelector('.author').value,
+    text: detailsModal.querySelector('.comment-text').value,
+  }
 
-   idea.comments.push(comment);
-   localStorage.setItem('ideas', JSON.stringify(ideas));
-   loadDetails(ideaNumber);
- }
- //
- let ideaWindow = document.getElementById("idea-creation-modal");
- let ideaBtn = ideaWindow.querySelector("#submit-idea");
+  idea.comments.push(comment);
+  localStorage.setItem('ideas', JSON.stringify(ideas));
+  loadDetails(ideaNumber);
+}
 
-// Enlever le disable button idea create
- ideaWindow.querySelector(".idea-name").addEventListener("input", updateSubmitButtonState);
- ideaWindow.querySelector(".detailed-description").addEventListener("input", updateSubmitButtonState);
- ideaWindow.querySelector(".idea-description").addEventListener("input", updateSubmitButtonState);
+/*---------------
+ * Update sumbit button
+ *---------------*/
 
- function updateSubmitButtonState(){
-   console.log("Event lancé");
-   if(ideaWindow.querySelector(".idea-name").value === "" ||
-      ideaWindow.querySelector(".detailed-description").value === "" ||
-      ideaWindow.querySelector(".idea-description").value === "")
-   {
-     ideaBtn.disabled = true;
-   } else {
-     ideaBtn.disabled = false;
-   }
- }
+let ideaWindow = document.getElementById("idea-creation-modal");
+let ideaBtn = ideaWindow.querySelector("#submit-idea");
+
+function updateSubmitButtonState(){
+  if(ideaWindow.querySelector(".idea-name").value === "" ||
+     ideaWindow.querySelector(".detailed-description").value === "" ||
+     ideaWindow.querySelector(".idea-description").value === "")
+  {
+    ideaBtn.disabled = true;
+  } else {
+    ideaBtn.disabled = false;
+  }
+}
+
+ideaWindow.querySelector(".idea-name").addEventListener("input", updateSubmitButtonState);
+ideaWindow.querySelector(".detailed-description").addEventListener("input", updateSubmitButtonState);
+ideaWindow.querySelector(".idea-description").addEventListener("input", updateSubmitButtonState);
